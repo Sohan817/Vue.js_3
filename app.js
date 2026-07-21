adcab23b9c4af7ea792b45dca45ff5e9d719dcb3
Shohan <hsohan509@gmail.com>
2026-07-22 02:16:59 +0600

    v-for

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      bookName: "The Alchemist",
-      author: "Paulo Coelho",
-      age: 1988,
-      x: 0,
-      y: 0,
+      // bookName: "The Alchemist",
+      // author: "Paulo Coelho",
+      // age: 1988,
+      // x: 0,
+      // y: 0,
+      books: [
+        { name: "The Alchemist", author: "Paulo Coelho" },
+        { name: "1984", author: "George Orwell" },
+        { name: "To Kill a Mockingbird", author: "Harper Lee" },
+      ],
     };
   },
   methods: {
@@ -19,16 +24,16 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleMouseEvent(e, data) {
-      console.log(e);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-    },
+    // handleMouseEvent(e, data) {
+    //   console.log(e);
+    //   if (data) {
+    //     console.log(data);
+    //   }
+    // },
+    // handleMouseMove(e) {
+    //   this.x = e.offsetX;
+    //   this.y = e.offsetY;
+    // },
   },
 });
 app.mount("#app");
