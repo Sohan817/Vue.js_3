ec09515ed73c348986f79f30a4ddbcd721c27e72
Shohan <hsohan509@gmail.com>
2026-08-01 03:08:39 +0600

    Router-link

--- jobs_router/src/views/AboutView.vue
+++ jobs_router/src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p class="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam autem,
+      aliquid blanditiis deleniti accusamus corporis illum est ipsum iste animi
+      distinctio nisi explicabo et velit cumque sequi ut perspiciatis hic nobis
+      ipsa perferendis facere! Modi sed minima obcaecati odit numquam eveniet
+      ullam ab minus! Eveniet, magnam, mollitia non sunt ad aperiam adipisci,
+      temporibus ea natus maxime doloremque voluptate placeat quisquam illum
+      fugit pariatur modi voluptatum tempore libero exercitationem! Doloribus,
+      placeat perspiciatis! Error temporibus dolorum excepturi saepe obcaecati
+      eaque impedit, accusantium facilis ea at aperiam, cum voluptatibus modi
+      hic optio itaque veritatis perspiciatis, necessitatibus facere! Impedit
+      deleniti dolorem itaque blanditiis a.
+    </p>
   </div>
 </template>
--- jobs_router/src/views/HomeView.vue
+++ jobs_router/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+      accusamus rerum suscipit excepturi id vel alias iste exercitationem
+      veniam, ipsa iure nam, ipsam est voluptates nemo labore minus soluta, quae
+      beatae placeat? Illo perspiciatis unde ut distinctio sit, laboriosam
+      delectus ratione repudiandae labore quam suscipit recusandae voluptatibus
+      quo necessitatibus eaque accusamus modi. Voluptate, sit? Quod architecto
+      vero ex, ipsa laudantium optio odit corrupti tempora quasi magni fugiat et
+      harum voluptatibus doloremque illum incidunt quas ipsam ad temporibus
+      itaque? Voluptas dicta officiis quos eligendi. Doloremque asperiores quos
+      corporis fugit reprehenderit perspiciatis repudiandae dolorem, reiciendis
+      molestias voluptates! Fuga unde aliquam perferendis amet.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+      accusamus rerum suscipit excepturi id vel alias iste exercitationem
+      veniam, ipsa iure nam, ipsam est voluptates nemo labore minus soluta, quae
+      beatae placeat? Illo perspiciatis unde ut distinctio sit, laboriosam
+      delectus ratione repudiandae labore quam suscipit recusandae voluptatibus
+      quo necessitatibus eaque accusamus modi. Voluptate, sit? Quod architecto
+      vero ex, ipsa laudantium optio odit corrupti tempora quasi magni fugiat et
+      harum voluptatibus doloremque illum incidunt quas ipsam ad temporibus
+      itaque? Voluptas dicta officiis quos eligendi. Doloremque asperiores quos
+      corporis fugit reprehenderit perspiciatis repudiandae dolorem, reiciendis
+      molestias voluptates! Fuga unde aliquam perferendis amet.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
