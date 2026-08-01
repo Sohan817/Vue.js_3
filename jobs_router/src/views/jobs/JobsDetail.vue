b58b35ef5781efad5697031596c9a1b24260f9f4
Shohan <hsohan509@gmail.com>
2026-08-02 03:40:46 +0600

    Route parameter

---
+++ jobs_router/src/views/jobs/JobsDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Job Details</h1>
+  <p>Jobs detail id: {{ jobId }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobId: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style></style>
