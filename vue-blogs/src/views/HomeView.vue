2f8bc0de52efb030025b311ff6406aac125748cc
Shohan <hsohan509@gmail.com>
2026-08-04 03:03:59 +0600

    Composition API basics

---
+++ vue-blogs/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">My name is {{ name }} and I am {{ age }} years old.</div>
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  setup() {
+    let name = "Shohan";
+    let age = 20;
+    const handleClick = () => {
+      console.log("Button clicked!");
+    };
+    return {
+      name,
+      age,
+      handleClick,
+    };
+  },
+};
+</script>
