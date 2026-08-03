dc16c1b83f984889f55be92227206f0e861106d0
Shohan <hsohan509@gmail.com>
2026-08-04 03:32:33 +0600

    Templete refs in composition api

--- vue-blogs/src/views/HomeView.vue
+++ vue-blogs/src/views/HomeView.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="home">My name is {{ name }} and I am {{ age }} years old.</div>
+  <div ref="divRef" class="home">
+    My name is {{ name }} and I am {{ age }} years old.
+  </div>
   <button @click="handleClick">Click Me</button>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "HomeView",
   setup() {
     let name = "Shohan";
     let age = 20;
+    const divRef = ref(null);
+    console.log(divRef, divRef.value); // This will log the DOM element after the component is mounted
+
     const handleClick = () => {
-      console.log("Button clicked!");
+      console.log(divRef, divRef.value); // This will log the DOM element when the button is clicked
+      divRef.value.classList.add("clicked");
+      divRef.value.textContent = "Hello Refs"; // This will add a class to the div when the button is clicked
     };
     return {
       name,
       age,
+      divRef,
       handleClick,
     };
   },
