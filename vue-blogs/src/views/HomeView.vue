15217fee5795e0770ab698d6eb9567157dffb574
Shohan <hsohan509@gmail.com>
2026-08-07 01:53:51 +0600

    Watch & watchEffect

--- vue-blogs/src/views/HomeView.vue
+++ vue-blogs/src/views/HomeView.vue
@@ -3,21 +3,34 @@
   <input v-model="search" placeholder="Search names..." />
   <p>{{ search }}</p>
   <div v-for="name in searchedName" :key="name">{{ name }}</div>
+  <button @click="handleWatchStop">Stop Watch</button>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 export default {
   name: "HomeView",
   setup() {
     const search = ref("");
     const names = ref(["John", "Doe", "Sohan", "Rohit", "Ramesh", "Suresh"]);
 
+    const stopWatch = watch(search, () => {
+      console.log("Search value changed to:");
+    });
+
+    const stopWatchEffect = watchEffect(() => {
+      console.log("Search value changed to:", search.value);
+    });
+
     const searchedName = computed(() => {
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { search, names, searchedName };
+    const handleWatchStop = () => {
+      stopWatch();
+      stopWatchEffect();
+    };
+    return { search, names, searchedName, handleWatchStop };
   },
 };
 </script>
