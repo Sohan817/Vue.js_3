90bbc213491596110b762048d283f9b5bcfd2786
Shohan <hsohan509@gmail.com>
2026-08-19 21:54:10 +0600

    Props

--- vue-courses/src/App.vue
+++ vue-courses/src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <h1 class="text-4xl font-medium">Course Hub</h1>
-  <h2 class="text-2xl font-medium">All Courses</h2>
-  <h2 class="text-2xl font-medium">Your Courses</h2>
+  <div class="p-4 space-y-8">
+    <h1 class="text-4xl font-medium">Course Hub</h1>
+    <h2 class="text-2xl font-medium">All Courses</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <CourseItem
+        v-for="i in 3"
+        :key="i"
+        Title="Vue Courses"
+        Price="9.9"
+        Description="0 to Hero vue course"
+      ></CourseItem>
+    </div>
+    <h2 class="text-2xl font-medium">Your Courses</h2>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import CourseItem from "./components/CourseItem.vue";
+</script>
---
+++ vue-courses/src/components/CourseItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-white border border-gray-200 rounded-md">
+    <h1 class="p-4 text-xl font-medium border-b border-gray-200">
+      {{ Title }}
+    </h1>
+    <p class="p-4 border-b border-gray-200">{{ Price }}</p>
+    <p class="p-4">{{ Description }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  Title: String,
+  Price: Number,
+  Description: String,
+});
+</script>
