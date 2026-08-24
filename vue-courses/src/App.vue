214ac50776d51a4036b0e20c2fd86f033bc73086
Shohan <hsohan509@gmail.com>
2026-08-24 21:02:38 +0600

    Book a courses

--- vue-courses/src/App.vue
+++ vue-courses/src/App.vue
@@ -2,14 +2,16 @@
   <div class="p-4 space-y-8">
     <h1 class="text-4xl font-medium">Course Hub</h1>
     <h2 class="text-2xl font-medium">All Courses</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div v-if="error">Failed to load data</div>
+    <div v-else-if="loading"><SkeletonCourses v-for="i in 4" :key="i" /></div>
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <CourseItem
         v-for="course in courses"
         :key="course.id"
         :Title="course.title"
         :Price="course.price"
         :Description="course.description"
-        @click="console.log('button clicked')"
+        @click="bookCourses(course)"
       ></CourseItem>
     </div>
     <h2 class="text-2xl font-medium">Your Courses</h2>
@@ -19,21 +21,51 @@
 
 <script setup>
 import CourseItem from "./components/CourseItem.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import BookingItem from "./components/BookingItem.vue";
+import SkeletonCourses from "./components/SkeletonCourses.vue";
 const error = ref("");
 const courses = ref([]);
+const loading = ref(false);
+
+//Fetch data from database
 const fetchData = async () => {
+  loading.value = true;
   try {
     const response = await fetch("http://localhost:3000/courses");
     courses.value = await response.json();
     if (!response.ok) {
       throw new Error("Faild to load data");
     }
-    console.log(datas);
+    loading.value = false;
   } catch (e) {
     error.value = e.message;
   }
 };
-fetchData();
+
+//Booking courses
+const bookCourses = async (course) => {
+  const bookedCourses = {
+    id: Date.now().toString(),
+    title: course.title,
+    price: course.price,
+    description: course.description,
+    status: "pending",
+  };
+  try {
+    const response = await fetch(" http://localhost:3000/bookings", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bookedCourses),
+    });
+    if (!response.ok) {
+      throw new Error("Faild to load data");
+    }
+  } catch (e) {
+    error.value = e.message;
+  }
+};
+onMounted(() => {
+  fetchData();
+});
 </script>
---
+++ vue-courses/src/components/SkeletonCourses.vue
@@ -0,0 +1,14 @@
+<template>
+  <RoundedCart>
+    <div class="flex flex-col justify-between border border-gray-200">
+      <div class="h-36 bg-gray-300"></div>
+      <div class="p-4 flex items-center h-18 justify-between">
+        <div class="bg-gray-300 rounded-full h-8 w-36"></div>
+        <div class="bg-gray-300 rounded-full h-6 w-36"></div>
+      </div>
+    </div>
+  </RoundedCart>
+</template>
+<script>
+import RoundedCart from "./RoundedCart.vue";
+</script>
