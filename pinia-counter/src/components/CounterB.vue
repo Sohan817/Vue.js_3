783fcaba3e1fac99a0e5855447bd61e4f5bdde75
Shohan <hsohan509@gmail.com>
2026-09-08 00:49:07 +0600

    install and pluged in pinia

---
+++ pinia-counter/src/components/CounterB.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Counter B</h1>
+    <p>{{ count }}</p>
+    <button @click="count++">Increment</button>
+    <button @click="count--">Decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
