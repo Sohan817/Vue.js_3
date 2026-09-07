bd5d9611ada5ceab7c28b04ecf597bdb1ce639d1
Shohan <hsohan509@gmail.com>
2026-09-08 02:24:59 +0600

    Setup pinia-tasks

---
+++ pinia-tasks/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <!-- Main content goes here -->
+    <header>
+      <img src="../src/assets/pinia-logo.svg" alt="Pinia Logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
