b0302c62e2321ce3afb5523d36942c6f8aac999c
Shohan <hsohan509@gmail.com>
2026-09-13 02:23:12 +0600

    Setup shop-router

---
+++ shop-router/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>About our shop with Vue Router</h1>
+</template>
+
+<script></script>
---
+++ shop-router/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Welcome to Your shop with Vue Router</h1>
+</template>
+
+<script></script>
