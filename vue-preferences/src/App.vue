b301d8465f9752547bb778efdb7a99c7554fb797
Shohan <hsohan509@gmail.com>
2026-09-18 23:40:20 +0600

    Rendering tabs and css

--- vue-preferences/src/App.vue
+++ vue-preferences/src/App.vue
@@ -3,9 +3,13 @@
     <nav
       class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4"
     >
-      <ul class="flex flex-warp">
+      <ul class="flex flex-warp -mb-px">
         <li v-for="tab in tabs" :key="tab.key">
-          <TabButton :tab="tab" :current-tab="currentTab"></TabButton>
+          <TabButton
+            @click="currentTab = tab.key"
+            :tab="tab"
+            :current-tab="currentTab"
+          ></TabButton>
         </li>
       </ul>
     </nav>
--- vue-preferences/src/components/TabButton.vue
+++ vue-preferences/src/components/TabButton.vue
@@ -1,8 +1,21 @@
-<template>{{ tab.label }}</template>
+<template>
+  <button
+    @click="$emit('click')"
+    :class="{
+      'p-4 border-b-2': true,
+      'border-transparent': !isActive,
+      'border-b-blue-500 text-blue-500': isActive,
+    }"
+  >
+    {{ tab.label }}
+  </button>
+</template>
 <script setup>
 import { Tab } from "@/types";
+import { computed } from "vue";
 
-defineProps({
+const isActive = computed(() => props.currentTab === props.tab.key);
+const props = defineProps({
   currentTab: String,
   tab: Tab,
 });
